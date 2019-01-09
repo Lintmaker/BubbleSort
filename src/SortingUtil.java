860dcf199562c17265be792024e08dae319660e4
BT_1N3_16 <lpan7932@bths.edu>
2019-01-09 14:49:46 -0500

    Not done with bubbleSortString

--- src/SortingUtil.java
+++ src/SortingUtil.java
@@ -7,6 +7,14 @@ public class SortingUtil
         arr[j]=temp;
 
     }
+    public static void swapString(String[] arr, int i, int j)
+    {
+        String temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+    }
+
     public static boolean checkSum(int [] arr)
     {
         int sum = 0;
@@ -51,6 +59,30 @@ public class SortingUtil
         }
         return arr;
     }
+    public static String[] bubbleSortString(String[] arr)
+    {
+        boolean sorted= SortingUtil.isSortedString(arr);
+        int lastSwap = arr.length;
+        int temp=0;
+        while(!sorted)
+        {
+            for(int i=0; i<lastSwap; i++)
+            {
+                if(i+1!=arr.length)
+                {
+                    if (arr[i].compareTo(arr[i + 1]) == -1)
+                    {
+                        SortingUtil.swapString(arr, i, i + 1);
+                    }
+                }
+                temp=i;
+            }
+            lastSwap=temp;
+            sorted=SortingUtil.isSortedString(arr);
+
+        }
+        return arr;
+    }
     public static int [] selectionSort (int [] arr)
     {
         int minValue = 0;
@@ -72,12 +104,26 @@ public class SortingUtil
         }
         return arr;
     }
+
     public static boolean isSorted(int[] arr)
     {
         boolean result=true;
         for(int i =0; i <arr.length-1; i++)
         {
             if(arr[i] > arr[i+1])
+            {
+                result=false;
+                break; //if there is an error, it breaks the loop and returns false for it to fix. Otherwise if the last value is an error it will be true instead of false.
+            }
+        }
+        return result;
+    }
+    public static boolean isSortedString(String[] arr)
+    {
+        boolean result=true;
+        for(int i =0; i <arr.length-1; i++)
+        {
+            if(arr[i].compareTo(arr[i+1]) == -1);
             {
                 result=false;
                 break; //if there is an error, it breaks the loop and returns false for it to fix. Otherwise if the last value is an error it will be true instead of false.
@@ -96,6 +142,23 @@ public class SortingUtil
         return arr;
     }
 
+    public static String[] randomStringArr(int num, int length){
+        String[] arr = new String[num];
+        while(num > 0){
+            int i = 0;
+            String s = "";
+            while(i < length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s += c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+    }
+
     public static String print(int[] arr)
     {
         String result="";
@@ -103,6 +166,16 @@ public class SortingUtil
         for(int num: arr)
         {
             result += " " + num;
+        }
+        return result;
+    }
+    public static String printString(String[] arr)
+    {
+        String result="";
+
+        for(int num = 0; num < arr.length; num++)
+        {
+            result += arr[num] + " ";
         }
         return result;
     }
